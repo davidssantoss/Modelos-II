77c248f87333a55a76f6a9edf46b865c8454ebe1
Laurent Chaverra <ldmckkb@gmail.com>
2019-07-23 04:34:20 -0500

    update

--- FRIV/games_platform/urls.py
+++ FRIV/games_platform/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'games_platform'
 urlpatterns = [    
-    path('', views.index, name='index'),
+    path(' ', views.index, name='index'),
     path('register', views.register, name = 'register'),
     path('login', views.login, name = 'login')    
 ]
