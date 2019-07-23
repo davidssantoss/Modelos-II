44a6b9fd0bd58c164b339429f60dd8e410f6dc40
davidssantoss <davidsteven.santos9@gmail.com>
2019-07-23 04:26:37 -0500

    menu juegos add

--- FRIV/games_platform/urls.py
+++ FRIV/games_platform/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 app_name = 'games_platform'
 urlpatterns = [    
+    path('', views.MenuView.as_view(), name = 'menu'),
     path('register', views.register, name = 'register'),
-    path('login', views.login, name = 'login')
+    path('login', views.login, name = 'login'),
+    path('Arkanoid', views.arkanoid, name = 'arkanoid'),
+    path('CarGame', views.car_game, name = 'car_game'),
+    path('Chicken', views.chicken, name = 'chicken'),
+    path('CrossyRoad', views.crossyroad, name = 'crossyroad'),
+    path('HeadGame', views.head_game, name = 'head_game'),
+    path('Penguin', views.penguin, name = 'Penguin'),
+    path('ShipGame', views.ship_game, name = 'ship_game'),
+    path('Snake', views.snake, name = 'snake'),
 ]
--- FRIV/games_platform/views.py
+++ FRIV/games_platform/views.py
@@ -1,25 +1,70 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 from . import models
+from .models import *
+
 
 def register(request):
     if (request.method == 'POST'):
-        usernameDB = request.POST['username']                        
-        if (models.Player.objects.filter(pk = usernameDB).exists()):
-            return render(request, 'games_platform/register.html', {'error_message':'el usuario ya existe'})
+        usernameDB = request.POST['username']
+        if (models.Player.objects.filter(pk=usernameDB).exists()):
+            return render(request, 'games_platform/register.html', {'error_message': 'el usuario ya existe'})
         else:
-            models.Player(username = usernameDB, name = request.POST['name'], last_name = request.POST['last_name']).save()          
+            models.Player(
+                username=usernameDB, name=request.POST['name'], last_name=request.POST['last_name']).save()
             return render(request, 'games_platform/register.html')
     else:
         return render(request, 'games_platform/register.html')
 
+
 def login(request):
     if (request.method == 'POST'):
-        usernameDB = request.POST['username']                        
-        if (models.Player.objects.filter(pk = usernameDB).exists()):
+        usernameDB = request.POST['username']
+        if (models.Player.objects.filter(pk=usernameDB).exists()):
             return HttpResponse('entrar a plataforma con %s registrado' % usernameDB)
         else:
             return render(request, 'games_platform/login.html', {'error_message': 'el usuario no esta registrado'})
     else:
         return render(request, 'games_platform/login.html')
+
+
+def arkanoid(request):
+    return render(request, 'games_platform/arkanoid.html')
+
+
+def crossyroad(request):
+    return render(request, 'games_platform/crossyroad.html')
+
+
+def car_game(request):
+    return render(request, 'games_platform/car_game.html')
+
+
+def chicken(request):
+    return render(request, 'games_platform/chicken.html')
+
+
+def head_game(request):
+    return render(request, 'games_platform/head_game.html')
+
+
+def penguin(request):
+    return render(request, 'games_platform/penguin.html')
+
+
+def ship_game(request):
+    return render(request, 'games_platform/ship_game.html')
+
+
+def snake(request):
+    return render(request, 'games_platform/snake.html')
+
+class MenuView(generic.ListView):
+    template_name = 'games_platform/menu.html'
+    context_object_name = 'game_list'
+
+    def get_queryset(self):
+        return Game.objects.order_by('-name')
+
 
